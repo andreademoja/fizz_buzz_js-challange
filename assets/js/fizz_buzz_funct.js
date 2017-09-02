9c15e30ba2d139a26309d3043c83f23923e2df53
Andrea <andreademoja@gmail.com>
2017-09-02 12:43:50 +0200

    added return fizz for num divisible by 3

---
+++ assets/js/fizz_buzz_funct.js
@@ -0,0 +1,9 @@
+function hasZeroReminder(number, dividend) {
+  return number % dividend == 0;
+}
+
+function fizzBuzz(number) {
+  if (number % 3 == 0) {
+    return "Fizz";
+  }
+}
--- spec/fizz_buzz_spec.js
+++ spec/fizz_buzz_spec.js
@@ -1,6 +1,11 @@
 describe("fizzBuzz", function() {
 
-  it("Check if number has zero reminder", function() {
+  it("should return 0", function() {
       expect(hasZeroReminder(3, 3)).toBe(true);
   });
+
+  it("should return 'Fizz' fo number divisible by 3", function (){
+      expect(fizzBuzz(96)).toBe("Fizz")
+  })
+
 });
